fd1d16195d39c26adc153ef85889c5c5ddd55e07
Scoopsies <mooninites103@gmail.com>
2023-10-06 15:12:20 -0500

    Created and set price on users table for backend

--- server/db/products.js
+++ server/db/products.js
@@ -13,9 +13,9 @@ const fetchProducts = async()=> {
 
 const createProduct = async(product)=> {
   const SQL = `
-    INSERT INTO products (id, name) VALUES($1, $2) RETURNING *
+    INSERT INTO products (id, name, price) VALUES($1, $2, $3) RETURNING *
   `;
-  const response = await client.query(SQL, [ uuidv4(), product.name]);
+  const response = await client.query(SQL, [ uuidv4(), product.name, product.price]);
   return response.rows[0];
 };
 
