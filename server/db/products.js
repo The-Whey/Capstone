764ed849ff8e36134e8135931f24c42c9e755e4a
Scoopsies <mooninites103@gmail.com>
2023-10-09 20:44:19 -0500

    Backend functionality working for products, also moved the default description to appropriate file

--- server/db/products.js
+++ server/db/products.js
@@ -11,15 +11,11 @@ const fetchProducts = async()=> {
   return response.rows;
 };
 
-
-// Creates a generic description for development
-const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ultrices lacus nec odio auctor, in congue lacus ultricies. Quisque non ligula et enim consequat scelerisque. Integer interdum leo tristique feugiat lobortis. Phasellus nunc erat, hendrerit vitae neque in, scelerisque convallis eros. Cras vitae purus bibendum, placerat lectus ut, consectetur arcu. Praesent porta, tellus dignissim cursus elementum, dolor ipsum iaculis purus, sed consequat erat magna et odio. In volutpat mi enim, eu tempus eros porta nec.'
-
 const createProduct = async(product)=> {
   const SQL = `
     INSERT INTO products (id, name, price, description) VALUES($1, $2, $3, $4) RETURNING *
   `;
-  const response = await client.query(SQL, [ uuidv4(), product.name, product.price, loremIpsum]);
+  const response = await client.query(SQL, [ uuidv4(), product.name, product.price, product.description]);
   return response.rows[0];
 };
 
