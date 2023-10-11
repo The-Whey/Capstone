60fcda32c6c1491af0ea3f4f410c14dcec4675bf
Scoopsies <mooninites103@gmail.com>
2023-10-10 22:14:39 -0500

    Total Price showing up for each order

--- src/Orders.js
+++ src/Orders.js
@@ -9,7 +9,7 @@ const Orders = ({ orders, products, lineItems })=> {
         {
           orders.filter(order => !order.is_cart).map( order => {
             const orderLineItems = lineItems.filter(lineItem => lineItem.order_id === order.id);
-
+            let total = 0;
             return (
               <li key={ order.id }>
                 ({ new Date(order.created_at).toLocaleString() })
@@ -18,16 +18,20 @@ const Orders = ({ orders, products, lineItems })=> {
                     orderLineItems.map( lineItem => {
                       const product = products.find(product => product.id === lineItem.product_id);
                       if (!product) return null;
-                      
+                      total += lineItem.quantity * product.price
+                      console.log(total)
                       
                       return (
                         <li key={ lineItem.id }>
                           <Link to={`/products/${product.id}`}>{product.name}</Link> ({lineItem.quantity}) at ${(product.price / 100).toFixed(2)} each
+                          
                         </li>
                       );
                     })
+                    
                   }
                 </ul>
+                <h4>{`Total Price: $${(total / 100).toFixed(2)}`}</h4>
               </li>
             );
           })
