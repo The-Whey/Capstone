f11f4ca864cac3404954379e99af2c4f8b468132
Scoopsies <mooninites103@gmail.com>
2023-10-11 15:43:42 -0500

    created a profile page that displays username, and vip status, will circle back to make things changeable

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Profile = ({auth, users}) => {
-    console.log(auth)
-    console.log(users)
+
   return (
     <div>
-        <h1>This is profile page</h1>
+        <h2>Profile Settings</h2>
+        <h4>{`Username: ${auth.username}`}</h4>
+        <h4>{auth.is_vip ? 'Thanks for being a vip!' : 'Upgrade to our VIP club for exclusive deals and promotions.'}</h4>
     </div>
   )
 }
