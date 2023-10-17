088233041851e2486aa366f037b95a0434c3d9a1
Scoopsies <mooninites103@gmail.com>
2023-10-16 22:19:01 -0500

    images as links in wishlists

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import Products from './Products';
 
 const Wishlist = ({bookmarks, products, cartItems, createLineItem, removeBookmark}) => {
     const navigate = useNavigate();
